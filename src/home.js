d6a67cd2ba0d0bf6481478b14218e82db2e708c8
Amy Boyer <amy@zooniverse.org>
2016-10-17 15:44:42 -0500

    get react router working

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './home.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
       <div className="App">
@@ -11,11 +11,11 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/Home.js</code> and save to reload.
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
+
+import './index.css';
+import Home from './home';
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path='/' component={Home}>
+          <Route path='/aaa' component={Home}/>
+        </Route>
+        {/* <Route path='/address' component={Address} /> */}
+      </Router>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
